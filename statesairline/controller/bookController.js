03abd8e5ece9553611966c67d63cadc38c07f092
Parkstelth <stelthicmaster@gmail.com>
2021-10-13 15:55:08 +0900

    request.query? / request.params?

--- statesairline/controller/bookController.js
+++ statesairline/controller/bookController.js
@@ -7,7 +7,21 @@ module.exports = {
   // 전체 데이터 혹은 요청 된 flight_uuid, phone 값과 동일한 예약 데이터를 조회합니다.
   findById: async (req, res) => {
     //TODO: 
-    
+    let list = booking
+    if (req.query.flight_uuid!== undefined) {
+
+      list = list.filter((item) => {
+        return item.flight_uuid.includes(req.query.flight_uuid);
+      });
+      return res.status(200).json(list);
+    }
+    if (req.query.phone!== undefined) {
+
+      list = list.filter((item) => {
+        return item.phone.includes(req.query.phone);
+      });
+      return res.status(200).json(list[0]); //연습
+    }
 
     return res.status(200).json(booking);
   },
@@ -15,8 +29,8 @@ module.exports = {
   // [POST] /book 요청을 수행합니다.
   // 요청 된 예약 데이터를 저장합니다.
   create: async (req, res) => {
-    //TODO: 
-    
+   
+    booking.push(req.body)
     return res.status(201).json({ message: 'Create success!' });
   },
 
@@ -24,7 +38,9 @@ module.exports = {
   // 요청 된 phone 값과 동일한 예약 데이터를 삭제합니다.
   deleteById: async (req, res) => {
     //TODO: 
-
+    booking = booking.filter((item)=>{
+      return item.phone!==req.query.phone
+    })
     
     
     return res.status(200).json(booking);
--- statesairline/controller/flightController.js
+++ statesairline/controller/flightController.js
@@ -5,17 +5,53 @@ module.exports = {
   // 요청 된 departure_times, arrival_times, destination, departure 값과 동일한 값을 가진 항공편 데이터를 조회합니다.
   findAll: async (req, res) => {
     //TODO: 
+ 
 
+    let list = flights
+    if (req.query.departure_times !== undefined) {
 
+      list = flights.filter((item) => {
+        return item.departure_times.includes(req.query.departure_times);
+      });
+      // return res.status(200).json(list);
+    }
+    if (req.query.arrival_times !== undefined) {
 
-    return res.json(flights);
+      list = list.filter((item) => {
+        return item.arrival_times.includes(req.query.arrival_times);
+      });
+      // return res.status(200).json(list);
+    }
+    if (req.query.destination !== undefined) {
+
+      list = list.filter((item) => {
+        return item.destination.includes(req.query.destination);
+      });
+      // return res.status(200).json(list);
+    }
+    if (req.query.departure !== undefined) {
+
+      list = list.filter((item) => {
+        return item.departure.includes(req.query.departure);
+      });
+      // 
+    }
+    
+    return res.status(200).json(list);
   },
   // [GET] /flight/{:id}
   // 요청 된 id 값과 동일한 uuid 값을 가진 항공편 데이터를 조회합니다.
   findById: async (req, res) => {
     //TODO: 
-    
 
+    if (req.params.id!== undefined) {
+
+      list2 = flights.filter((item) => {
+        return item.uuid.includes(req.params.id);
+      });
+      // 
+    }
+    return res.status(200).json(list2);
 
   },
 
@@ -23,11 +59,28 @@ module.exports = {
   // 요청 된 id 값과 동일한 uuid 값을 가진 항공편 데이터를 요청 된 Body 데이터로 수정합니다.
   update: async (req, res) => {
     let data;
-    //TODO: 
-
+  
+    data = flights.filter((item)=>{
+      return item.uuid===req.params.id
+    }) //일치하는 데이타
     
+    // [{
+    //   uuid: 'af6fa55c-da65-47dd-af23-578fdba40bed',
+    //   departure: 'ICN',
+    //   destination: 'CJU',
+    //   departure_times: '2021-12-02T12:00:00',
+    //   arrival_times: '2021-12-03T12:00:00'
+    // }]
+      data =data.map((item)=>{
+      if(req.body.departure!==undefined) item.departure=req.body.departure
 
+      if(req.body.destination!==undefined) item.destination=req.body.destination
+      
+      if(req.body.departure_times!==undefined) item.departure_times=req.body.departure_times
+      if(req.body.arrival_times!==undefined) item.arrival_times=req.body.arrival_times
+      return item
+    })
     
-    return res.status(200).json(data);
+    return res.status(200).json(data[0]);
   }
 };
